d39801a06b3d8beb81a31971725bf58b09a7caba
03san-smz <genius@GeniusnoMacBook-puro.local>
2019-02-15 12:05:12 +0900

    Add group view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,21 @@
 class GroupsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    Group.create(name: group_params[:name], user_id: current_user.id)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def group_params
+    params.permit(:name)
+  end
+
 end
