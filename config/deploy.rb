834d29e04b21ea80cd8cdaf794b9bdc036d056d5
03san-smz <m-shimizu@mc-coltd.com>
2019-03-07 18:42:59 +0900

    Character change

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock '~> 3.11.0'
 
-set :application, "chat-space"
-set :repo_url, "git@github.com:03san-smz/chat-space.git"
+set :application, 'chat-space'
+set :repo_url, 'git@github.com:03san-smz/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -12,8 +12,8 @@ set :rbenv_ruby, '2.3.1'
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/Users/genius/.ssh/testing-03san_smz.pem']
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :unicorn_pid, -> { '#{shared_path}/tmp/pids/unicorn.pid' }
+set :unicorn_config_path, -> { '#{current_path}/config/unicorn.rb' }
 set :keep_releases, 5
 
 after 'deploy:publishing', 'deploy:restart'
