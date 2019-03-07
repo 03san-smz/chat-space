8a4d7054baaeefff927bfbc3b8af4c1b65a80ffd
03san-smz <m-shimizu@mc-coltd.com>
2019-03-08 00:05:31 +0900

    Edit unicorn_deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '13.114.144.216', user: 'ec2-user', roles: %w{app db web}
+server '13.112.12.56', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
